e7df77299c32ff4ef1dcfd55b1b4635dfa9d284b
SergioMerinoCortez <utp0001553@alumno.utpuebla.edu.mx>
2024-04-26 11:17:47 -0600

    paso al video 123

--- contactos/src/main/java/smcdev/contactos/controlador/ContactoControlador.java
+++ contactos/src/main/java/smcdev/contactos/controlador/ContactoControlador.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.slf4j.Logger;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import smcdev.contactos.modelo.Contacto;
 import smcdev.contactos.servicio.ContactoServicio;
 
@@ -14,24 +17,45 @@ import java.util.List;
 
 @Controller
 public class ContactoControlador {
-/*enviando la informacion desde la clase controlador */
-private static final Logger logger = LoggerFactory.getLogger(ContactoControlador.class);
+    /*enviando la informacion desde la clase controlador */
+    private static final Logger logger = LoggerFactory.getLogger(ContactoControlador.class);
 
-@Autowired
+    @Autowired
     ContactoServicio contactoServicio;
 
-@GetMapping("/")
-    public String iniciar(ModelMap modelo){
-    List<Contacto> contactos = contactoServicio.ListarContactos();
-    contactos.forEach((contacto) -> logger.info(contactos.toString()));
-  modelo.put("contactos", contactos);
-    return "index";
+    @GetMapping("/")
+    public String iniciar(ModelMap modelo) {
+        List<Contacto> contactos = contactoServicio.ListarContactos();
+        contactos.forEach((contacto) -> logger.info(contactos.toString()));
+        modelo.put("contactos", contactos);
+        return "index";
+    }
+
+    @GetMapping("/agregar")
+    public String mostrarAgregar() {
+        return "agregar";
+    }
+
+
+    @PostMapping("/agregar")
+    public String agregar(@ModelAttribute("contactoForma")Contacto contacto){
+        logger.info("contacto a ageregar: " +  contacto);
+        contactoServicio.guardarContacto(contacto);
+         return "redirect:/";
+    }
+
+@GetMapping("/editar/{id}")
+
+  public String mostrarEditar(@PathVariable(value="id") int idContacto, ModelMap modelo)  {
+        Contacto contacto = contactoServicio.buscarContactoPorId(idContacto);
+        modelo.put("contacto",contacto);
+
+        return "editar";
 }
 
-@GetMapping("/agregar")
-    public String mostrarAgregar(){
-    return "agregar";
 }
 
 
-}
+
+
+
